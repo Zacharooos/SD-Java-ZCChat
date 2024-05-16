ba7e4ab00c9e0959d6b39d1e6888d50ab7014549
Zacharos <gabrielcesarfernandescarvalho@gmail.com>
2024-05-16 02:16:28 -0300

    Ajustes na classe Mensagem e remocao de classes prototipo

---
+++ zcchat/Mensagem.java
@@ -0,0 +1,39 @@
+package zcchat;
+
+import java.sql.Date;
+import java.util.UUID;
+
+public class Mensagem{
+    private final String id = UUID.randomUUID().toString().replace("-", "");
+    private final Date timestamp = new Date (System.currentTimeMillis());
+    private String text;
+    private Usuario emissor;
+    private Usuario destinatario;
+
+    public Mensagem(String text, Usuario emissor, Usuario destinatario) {
+        super();
+        this.text = text;
+        this.emissor = emissor;
+        this.destinatario = destinatario;
+    }
+    
+    public String get_id(){
+        return this.id;
+    }
+
+    public String get_text(){
+        return this.text;
+    }
+
+    public Usuario get_emissor(){
+        return this.emissor;
+    }
+    
+    public Usuario get_destinatario(){
+        return this.destinatario;
+    }
+
+    public Date get_timestamp(){
+        return this.timestamp;
+    }
+}
