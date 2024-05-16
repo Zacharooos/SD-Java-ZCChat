b1e4e2b4f8abe2845cef4f51f6280c0a1b23e235
Zacharos <gabrielcesarfernandescarvalho@gmail.com>
2024-05-15 21:10:47 -0300

    Subida de novo menu para HttpClient

--- zcchat/MyHttpClient.java
+++ zcchat/MyHttpClient.java
@@ -4,10 +4,136 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.util.Scanner;
 import java.net.URL;
 
 public class MyHttpClient {
-    public static void main(String[] args) throws IOException {
+
+	public static void client_menu() {
+		Scanner scanner = new Scanner(System.in);
+        int menu_index = -1;
+
+        while(menu_index != 0){
+            System.out.println("Oque você deseja fazer?\n * 1 - Procurar contatos online.\n * 2 - Falar com um contato online.\n * 3 - Visualizar historico.\n * 4 - Mensagem broadcast\n * 5 - Gerenciar conta\n Outra tecla para sair");
+
+            menu_index = scanner.nextInt();
+
+            switch (menu_index) {
+                case 1:
+                    System.out.println("Procurando contatos...");
+                    cliente_retrive_contacts_online();
+                    break;
+
+                case 2:
+                    // Precisa de outro switch para perguntar qual o contato da comunicação.
+                    System.out.println("Verificando se o contato está online...");
+                    cliente_message_contacts_online();
+                    break;
+
+                case 3:
+                    // Precisa enviar o nome do cliente que está estabelecendo o contato
+                    System.out.println("Buscando historico...");
+                    cliente_retrive_history();
+                    break;
+
+                case 4:
+                    // Precisa de outro switch para perguntar qual o contato da comunicação.
+                    System.out.println("Enviado mensagem offline...");
+                    cliente_message_all_contacts();
+                    break;
+
+                case 5:
+                    System.out.println("Acessando credenciais...");
+                    cliente_contact_info();
+                    break;
+
+                default:
+                    System.out.println("Saindo");
+                    menu_index = 0;
+                    break;
+            }
+        }
+
+            scanner.close();
+	}
+	
+    public static void cliente_retrive_contacts_online() {
+        try {
+            // Montando corpo da requisição
+            Payload objPayload = new Payload("retrieveContactsOnline", "Teste");
+            
+            // Enviando Payload e recebendo resposta
+            Payload responsePayloadObj = HttpConnect(objPayload);
+
+            System.out.println(responsePayloadObj.get("response"));
+        
+        } catch (Exception e) {
+            System.out.println("Erro na comunicação: " + e.toString());
+        }
+    }
+
+    public static void cliente_message_contacts_online() {
+        try {
+            // Montando corpo da requisição
+            Payload objPayload = new Payload("cliente_message_contacts_online", "Teste");
+            
+            // Enviando Payload e recebendo resposta
+            Payload responsePayloadObj = HttpConnect(objPayload);
+
+            System.out.println(responsePayloadObj.get("response"));
+        
+        } catch (Exception e) {
+            System.out.println("Erro na comunicação: " + e.toString());
+        }
+    }
+
+    public static void cliente_retrive_history() {
+        try {
+            // Montando corpo da requisição
+            Payload objPayload = new Payload("cliente_retrive_history", "Teste");
+            
+            // Enviando Payload e recebendo resposta
+            Payload responsePayloadObj = HttpConnect(objPayload);
+
+            System.out.println(responsePayloadObj.get("response"));
+        
+        } catch (Exception e) {
+            System.out.println("Erro na comunicação: " + e.toString());
+        }
+    }
+
+    public static void cliente_message_all_contacts() {
+        try {
+            // Montando corpo da requisição
+            Payload objPayload = new Payload("cliente_message_all_contacts", "Teste");
+            
+            // Enviando Payload e recebendo resposta
+            Payload responsePayloadObj = HttpConnect(objPayload);
+
+            System.out.println(responsePayloadObj.get("response"));
+        
+        } catch (Exception e) {
+            System.out.println("Erro na comunicação: " + e.toString());
+        }
+    }
+
+    public static void cliente_contact_info() {
+        try {
+            // Montando corpo da requisição
+            Payload objPayload = new Payload("cliente_contact_info", "Teste");
+            
+            // Enviando Payload e recebendo resposta
+            Payload responsePayloadObj = HttpConnect(objPayload);
+
+            System.out.println(responsePayloadObj.get("response"));
+        
+        } catch (Exception e) {
+            System.out.println("Erro na comunicação: " + e.toString());
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    public static Payload HttpConnect(Payload requestPayloadObj) throws IOException{
         // Configurando conexão
         String url = "http://localhost:8000/";
         HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
@@ -15,9 +141,7 @@ public class MyHttpClient {
         connection.setRequestProperty("Content-Type", "text/plain; charset=ISO-8859-1");
         connection.setDoOutput(true);
 
-        // Montando payload
-        Payload payloadObj = new Payload("teste", "argumento 1");
-        String requestBody = Payload.serializeHashMap(payloadObj);
+        String requestBody = Payload.serializeHashMap(requestPayloadObj);
 
         // Enviando para servidor
         // Escreve o corpo da requisição no fluxo de saída
@@ -26,7 +150,6 @@ public class MyHttpClient {
             outputStream.flush();
         }
 
-        
         // Obtém o body da resposta do servidor
         try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(),
         "ISO-8859-1"))) {
@@ -35,14 +158,23 @@ public class MyHttpClient {
             System.out.println("Response Code: " + responseCode);
 
             String responseString = Payload.decodeBufferFromSocket(br);
-            Payload responseObj = Payload.deserializeHashMap(responseString);
-            
+            Payload responsePayloadObj = Payload.deserializeHashMap(responseString);
+         
             // Imprimir o corpo da resposta
-            System.err.println(responseObj.get("response"));
+            System.err.println(responsePayloadObj.get("response"));
+            connection.disconnect();
+            return responsePayloadObj;
         }
 
-        // Fecha a conexão
-        connection.disconnect();
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        // TODO: Login
+
+        // Inicializa Login
+        client_menu();
+
 
     }
 }
